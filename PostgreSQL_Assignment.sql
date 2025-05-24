a04d88bf899fb6d74cdbc4da9dbc393fc3f04591
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-25 02:53:43 +0600

    solve all problem

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,11 +1,12 @@
 -- Active: 1747405528919@@127.0.0.1@5432@assignment2
--- Active: 1747405528919@@127.0.0.1@5432@assignment2-- Active: 1747405528919@@127.0.0.1@5432@assignment2
+
 
 CREATE Table rangers(
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     region VARCHAR(50) NOT NULL
 );
+
 
 
 CREATE Table species(
@@ -17,20 +18,23 @@ CREATE Table species(
 );
 
 
+
 CREATE TABLE sightings(
     sighting_id SERIAL PRIMARY KEY,
-    ranger_id INT REFERENCES rangers(ranger_id),
     species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
     location VARCHAR(100) NOT NULL,
     sighting_time TIMESTAMP NOT NULL,
     notes TEXT
 );
 
 
+
 INSERT INTO rangers (name, region) VALUES
 ('Alice Green', 'Northern Hills'),
 ('Bob White', 'River Delta'),
 ('Carol King', 'Mountain Range');
+
 
 
 INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
@@ -40,10 +44,83 @@ INSERT INTO species (common_name, scientific_name, discovery_date, conservation_
 ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
 
-INSERT INTO sightings (ranger_id, species_id, location, sighting_time, notes) VALUES
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
 (1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
 (2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
 (3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
 (1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL)
 
+
+
+-- Problem -1
+INSERT INTO rangers (name, region) VALUES('Derek Fox', 'Coastal Plains');
+
+
+
+-- Problem -2
+SELECT count(DISTINCT species_id) as unique_species_count FROM sightings;
+
+
+
+-- Problem -3
+SELECT * FROM sightings WHERE location ILIKE '%Pass%';
+
+
+
+-- Problem -4
+SELECT name, count(sighting_id) AS total_sightings FROM rangers
+LEFT JOIN sightings ON rangers.ranger_id = sightings.ranger_id 
+GROUP BY name 
+ORDER BY name;
+
+
+
+-- Problem -5
+SELECT common_name
+FROM species
+WHERE species_id NOT IN (SELECT species_id FROM sightings);
+
+
+
+-- Problem -6
+SELECT common_name, sighting_time, name FROM sightings        
+JOIN species ON sightings.species_id = species.species_id
+JOIN rangers ON sightings.ranger_id =  rangers.ranger_id
+ORDER BY sightings.sighting_time DESC
+LIMIT 2;
+
+
+
+-- Problem -7
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+
+
+-- Problem -8
+CREATE Function time_of_day(ts TIMESTAMP)
+RETURNS text
+LANGUAGE plpgsql
+AS
+$$
+   BEGIN
+        IF EXTRACT(HOUR FROM ts) < 12 THEN
+            RETURN 'Morning';
+        ELSIF EXTRACT(HOUR FROM ts) BETWEEN 12 AND 17 THEN
+            RETURN 'Afternoon';
+        ELSE
+            RETURN 'Evening';
+        END IF;
+    END;
+$$
+
+SELECT sighting_id, time_of_day(sighting_time) FROM sightings;
+
+
+
+-- Problem -9
+DELETE FROM rangers
+WHERE ranger_id NOT IN (SELECT DISTINCT ranger_id FROM sightings);
 
